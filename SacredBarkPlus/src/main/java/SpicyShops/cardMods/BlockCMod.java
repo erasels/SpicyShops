8ab9dfc7bc26271b9c3c3abbaa3f3e40ea2edc64
erasels <emanuelbedburdick@yahoo.de>
2020-07-11 22:34:50 +0200

    Added BlockCMod

--- SacredBarkPlus/src/main/java/SpicyShops/cardMods/BlockCMod.java
+++ SacredBarkPlus/src/main/java/SpicyShops/cardMods/BlockCMod.java
@@ -4,8 +4,8 @@ import SpicyShops.SpicyShops;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import org.apache.commons.lang3.math.NumberUtils;
 
-public class StrongCMod extends AbstractSpicySaleCMod{
-    public static final String ID = SpicyShops.getModID()+"Strong";
+public class BlockCMod extends AbstractSpicySaleCMod{
+    public static final String ID = SpicyShops.getModID()+"Block";
 
     @Override
     public String identifier(AbstractCard card) {
@@ -14,21 +14,21 @@ public class StrongCMod extends AbstractSpicySaleCMod{
 
     @Override
     public void onInitialApplication(AbstractCard card) {
-        card.baseDamage += 3 * NumberUtils.max(card.cost, 1);
+        card.baseBlock += 2 * NumberUtils.max(card.cost, 1);
     }
 
     @Override
     public float getPriceMod(AbstractCard c) {
-        return 1.5f;
+        return 1.35f;
     }
 
     @Override
     public boolean isApplicable(AbstractCard c) {
-        return c.baseDamage > -1;
+        return c.baseBlock > -1;
     }
 
     @Override
     public String getTexturePath() {
-        return "strong";
+        return "block";
     }
 }
