0bc3a0aabab96098cc5b688c14744e4f527768a0
erasels <emanuelbedburdick@yahoo.de>
2020-07-14 11:55:38 +0200

    Added curse bonus relic discount system

---
+++ SacredBarkPlus/src/main/java/SpicyShops/patches/SpicyRelicPatches.java
@@ -0,0 +1,82 @@
+package SpicyShops.patches;
+
+import SpicyShops.SpicyShops;
+import SpicyShops.util.HelperClass;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.shop.ShopScreen;
+import com.megacrit.cardcrawl.shop.StoreRelic;
+import com.megacrit.cardcrawl.vfx.FastCardObtainEffect;
+import javassist.CtBehavior;
+
+import java.util.ArrayList;
+
+public class SpicyRelicPatches {
+    private static final float CURSE_DISCOUNT_CHANCE = 0.99f;
+    private static final float CURSE_DISCOUNT = 0.66f;
+
+    @SpirePatch(clz = StoreRelic.class, method = SpirePatch.CLASS)
+    public static class ShopRelicFields {
+        public static SpireField<AbstractCard> bonusCard = new SpireField<>(() -> null);
+    }
+
+    @SpirePatch(clz = ShopScreen.class, method = "init")
+    public static class InitHook {
+        //Hook in after discounts are applied
+        @SpirePostfixPatch
+        public static void patch(ShopScreen __instance, ArrayList<AbstractCard> coloredCards, ArrayList<AbstractCard> colorlessCards, ArrayList<StoreRelic> ___relics) {
+            if(AbstractDungeon.merchantRng.randomBoolean(CURSE_DISCOUNT_CHANCE)) {
+                StoreRelic rel = HelperClass.getRandomItem(___relics, AbstractDungeon.merchantRng);
+                if(rel != null) {
+                    ShopRelicFields.bonusCard.set(rel, CardLibrary.getCard(HelperClass.getRandomItem(SpicyShops.vanillaCurses, AbstractDungeon.merchantRng)).makeCopy());
+                    SpicyShops.logger.info(rel.relic.name + " has a curse (" + ShopRelicFields.bonusCard.get(rel).name + ")attached to it for a discount. ");
+                    rel.price *= CURSE_DISCOUNT;
+                }
+            }
+        }
+    }
+
+    @SpirePatch(clz = StoreRelic.class, method = "render")
+    public static class RenderRelicSpecials {
+        @SpirePostfixPatch
+        public static void patch(StoreRelic __instance, SpriteBatch sb) {
+            AbstractCard c = ShopRelicFields.bonusCard.get(__instance);
+            if(c != null) {
+                if(c.type == AbstractCard.CardType.CURSE) {
+                    c.drawScale = __instance.relic.scale - 0.8f;
+                    //Draw card right of the relic if unhovered and left of it, if it is hovered, so that it doesn't overlap with the powertips
+                    c.current_x = __instance.relic.hb.x + (((__instance.relic.hb.width + (!__instance.relic.hb.hovered?(25f * Settings.scale):0)) * (__instance.relic.hb.hovered?-1f:1f)) * __instance.relic.scale);
+                    c.current_y = __instance.relic.hb.y + ((__instance.relic.hb.height/2f)*__instance.relic.scale);
+                    c.render(sb);
+                }
+            }
+        }
+    }
+
+    @SpirePatch(clz = StoreRelic.class, method = "purchaseRelic")
+    public static class BonusCardLogicOnRelicBuy {
+        @SpireInsertPatch(locator = Locator.class)
+        public static void patch(StoreRelic __instance) {
+            AbstractCard c = ShopRelicFields.bonusCard.get(__instance);
+            if(c != null) {
+                if(c.type == AbstractCard.CardType.CURSE) {
+                    AbstractDungeon.topLevelEffects.add(new FastCardObtainEffect(c, c.current_x, c.current_y));
+                    ShopRelicFields.bonusCard.set(__instance, null);
+                }
+            }
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+                Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractRelic.class, "flash");
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+            }
+        }
+    }
+}
