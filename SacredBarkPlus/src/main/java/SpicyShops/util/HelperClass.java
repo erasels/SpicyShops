1cf4c012e7b697dd000828b48bf2d9df55e4257e
erasels <emanuelbedburdick@yahoo.de>
2020-07-14 11:54:45 +0200

    Added getRandomItem helper function

--- SacredBarkPlus/src/main/java/SpicyShops/util/HelperClass.java
+++ SacredBarkPlus/src/main/java/SpicyShops/util/HelperClass.java
@@ -1,8 +1,15 @@
 package SpicyShops.util;
 
+import com.megacrit.cardcrawl.random.Random;
+
+import java.util.ArrayList;
+
 public class HelperClass {
     public static String capitalize(String str) {
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 
+    public static <T> T getRandomItem(ArrayList<T> list, Random rng) {
+        return list.isEmpty() ? null : list.get(rng.random(list.size() - 1));
+    }
 }
