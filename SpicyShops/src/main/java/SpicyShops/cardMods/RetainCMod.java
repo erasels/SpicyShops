f154189dcb7b49d122cd4e1064bd3965c35c4a8c
erasels <emanuelbedburdick@yahoo.de>
2021-07-13 20:03:38 +0200

    Fixed Retain CMod string formatting yet again

--- SpicyShops/src/main/java/SpicyShops/cardMods/RetainCMod.java
+++ SpicyShops/src/main/java/SpicyShops/cardMods/RetainCMod.java
@@ -25,12 +25,13 @@ public class RetainCMod extends AbstractSpicySaleCMod{
     public String modifyDescription(String rawDescription, AbstractCard card) {
         if(!CommonKeywordIconsField.useIcons.get(card)) {
             if (card.isInnate) {
+                int nlIndex = NumberUtils.max(rawDescription.indexOf("NL"), 0);
                 //Is innate in the bottom half of the card? Then don't care about trying to slot retain under it
-                if(rawDescription.indexOf(GameDictionary.INNATE.NAMES[0]) > (rawDescription.length()/2f)) {
+                if(rawDescription.indexOf(GameDictionary.INNATE.NAMES[0]) > (rawDescription.length()/2f) || nlIndex == 0) {
                     return HelperClass.capitalize(GameDictionary.RETAIN.NAMES[0]) + SPACE + LocalizedStrings.PERIOD + " NL " + rawDescription;
                 } else {
-                    String beforeNL = rawDescription.substring(0, NumberUtils.max(rawDescription.indexOf("NL"), 0));
-                    return beforeNL + "NL " + HelperClass.capitalize(GameDictionary.RETAIN.NAMES[0]) + SPACE + LocalizedStrings.PERIOD + " " + rawDescription.substring(rawDescription.indexOf("NL"));
+                    String beforeNL = rawDescription.substring(0, nlIndex);
+                    return beforeNL + "NL " + HelperClass.capitalize(GameDictionary.RETAIN.NAMES[0]) + SPACE + LocalizedStrings.PERIOD + " " + rawDescription.substring(nlIndex);
                 }
             } else {
                 return HelperClass.capitalize(GameDictionary.RETAIN.NAMES[0]) + SPACE + LocalizedStrings.PERIOD + " NL " + rawDescription;
